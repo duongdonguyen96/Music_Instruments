dbf556f7d30d7ce302ac0f227b5c00234e64cbfe
unknown <volong24071995>
2020-12-06 09:14:05 +0700

    updateService

--- Ungdungweb/project_module5/src/main/java/com/codegym/project/service/Impl/BannerService.java
+++
@@ -1,4 +0,0 @@
-package com.codegym.project.service.Impl;
-
-public class BannerService {
-}
--- Ungdungweb/project_module5/src/main/java/com/codegym/project/service/Impl/TypeProductServiceImpl.java
+++ Ungdungweb/project_module5/src/main/java/com/codegym/project/service/Impl/TypeProductServiceImpl.java
@@ -1,16 +1,20 @@
 package com.codegym.project.service.Impl;
 
 import com.codegym.project.model.TypeProduct;
+import com.codegym.project.repository.TypeProductRepository;
 import com.codegym.project.service.TypeProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.sql.SQLException;
 import java.util.List;
 @Service
 public class TypeProductServiceImpl implements TypeProductService {
+    @Autowired
+    TypeProductRepository typeProductRepository;
     @Override
     public List<TypeProduct> findAll() {
-        return null;
+        return typeProductRepository.findAll();
     }
 
     @Override
